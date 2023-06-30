07876a715908b53b84443c01998b542953dab04a
pei <peipeilong116@gmail.com>
2023-06-30 07:03:42 -0400

    feat: add bridge service

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,7 @@ export const Config = {
   checkoutSecureKey: process.env.CHECKOUT_SECURE_KEY,
   checkoutProcessingChannelId: process.env.CHECKOUT_PROCESSING_CHANNEL_ID,
   bridgeApiKey: process.env.BRIDGE_API_KEY,
+  bridgeApiURI: process.env.BRIDGE_API_URI,
   isProduction: process.env.NODE_ENV === 'production',
   isStaging: process.env.NODE_ENV === 'staging',
   frontendUri: process.env.FRONT_END_URI || 'https://test.checkout.mybackpack.app/',
